c34875812f7328373606c29df82a79cc57a13ca2
Viktor.Mironov <vicktor.mironov@gmail.com>
2017-10-13 18:01:11 +0500

    Альфа версия макета главной страницы

--- WPF-client/View/MainWindow.xaml.cs
+++ WPF-client/View/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -11,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+using WPF_client.Elements;
+using WPF_client.ViewModel;
 
 namespace WPF_client
 {
@@ -22,6 +26,30 @@ namespace WPF_client
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //until we had a StaysOpen glag to Drawer, this will help with scroll bars
+            var dependencyObject = Mouse.Captured as DependencyObject;
+            while (dependencyObject != null)
+            {
+                if (dependencyObject is ScrollBar) return;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            MenuToggleButton.IsChecked = false;
+        }
+
+        private async void MenuPopupButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var sampleMessageDialog = new SampleMessageDialog
+            {
+                Message = { Text = ((ButtonBase)sender).Content.ToString() }
+            };
+
+            await DialogHost.Show(sampleMessageDialog, "RootDialog");
         }
     }
 }
---
+++ WPF-client/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MaterialDesignThemes.Wpf;
+using WPF_client.Elements;
+
+namespace WPF_client.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            DemoItems = new DemoItem[] {};
+        }
+
+        public DemoItem[] DemoItems { get; }
+    }
+}
