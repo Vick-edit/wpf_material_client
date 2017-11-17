64672b163ba7f196f23c7b5670dffb4de6624161
Viktor.Mironov <vicktor.mironov@gmail.com>
2017-11-17 16:12:25 +0500

    Доработка графика

--- WPF-client/View/MainChart.xaml.cs
+++ WPF-client/View/MainChart.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts.Events;
+using WPF_client.ViewModel;
 
 namespace WPF_client.View
 {
@@ -24,6 +26,33 @@ namespace WPF_client.View
         {
             InitializeComponent();
             Background = Brushes.Transparent;
+        }
+
+        private void Axis_OnRangeChanged(RangeChangedEventArgs eventargs)
+        {
+            var vm = (MainChartViewModel)DataContext;
+
+            var currentRange = eventargs.Range;
+
+            if (currentRange < TimeSpan.TicksPerDay * 2)
+            {
+                vm.Formatter = x => new DateTime((long)x).ToString("t");
+                return;
+            }
+
+            if (currentRange < TimeSpan.TicksPerDay * 60)
+            {
+                vm.Formatter = x => new DateTime((long)x).ToString("dd MMM yy");
+                return;
+            }
+
+            if (currentRange < TimeSpan.TicksPerDay * 540)
+            {
+                vm.Formatter = x => new DateTime((long)x).ToString("MMM yy");
+                return;
+            }
+
+            vm.Formatter = x => new DateTime((long)x).ToString("yyyy");
         }
     }
 }
--- WPF-client/ViewModel/MainChartViewModel.cs
+++ WPF-client/ViewModel/MainChartViewModel.cs
@@ -1,25 +1,48 @@
-﻿using LiveCharts;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Helpers;
 using LiveCharts.Wpf;
 
 namespace WPF_client.ViewModel
 {
     public class MainChartViewModel
     {
-        public SeriesCollection DataCollection { get; }
+        public object Mapper { get; set; }
+        public Func<double, string> Formatter { get; set; }
+        public double From { get; set; }
+        public double To { get; set; }
 
         public MainChartViewModel()
         {
-            DataCollection = new SeriesCollection()
+            Values = new ChartValues<DateTimePoint>();
+
+            var trend = 50d;
+            var random = new Random();
+            var timeStep = DateTime.Now;
+            for (var i = 0; i < 500; i++)
             {
-                new LineSeries()
+                timeStep = timeStep.AddHours(1);
+                Values.Add(new DateTimePoint(timeStep.AddDays(i*10), trend));
+
+                if (random.NextDouble() > 0.4)
                 {
-                    Values = new ChartValues<double>() { 3, 5, 7, 4 }
-                },
-                new ColumnSeries()
-                {
-                    Values = new ChartValues<double>() { 5, 6, 2, 7 }
+                    trend += random.NextDouble() * 10;
                 }
-            };
+                else
+                {
+                    trend -= random.NextDouble() * 10;
+                }
+            }
+
+            Formatter = x => new DateTime((long)x).ToString("yyyy");
+            From = DateTime.Now.AddHours(10000).Ticks;
+            To = DateTime.Now.AddHours(90000).Ticks;
         }
+
+        public ChartValues<DateTimePoint> Values { get; set; }
     }
 }
