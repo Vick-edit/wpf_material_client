c513b4f7ec6351de72cdacbfe12a254bca15c88a
Viktor.Mironov <va.mironov@esbt.ru>
2018-02-12 22:09:24 +0500

    Добавлен временный файл конфига

--- WPF-client/Domain/ServerConnection/ServerUrl.cs
+++ WPF-client/Domain/ServerConnection/ServerUrl.cs
@@ -1,11 +1,31 @@
-﻿namespace WPF_client.Domain.ServerConnection
+﻿using System;
+using System.IO;
+using WPF_client.Utilities;
+
+namespace WPF_client.Domain.ServerConnection
 {
     public static class ServerUrl
     {
-        public static readonly string ServerName = "http://cbd4b7e9.ngrok.io";
+        public static readonly string ServerName = GetServerName();
 
         public static readonly string ForecastsObject = "localities";
 
         public static readonly string ForecastsData = "predict/{id}";
+
+        private static string GetServerName()
+        {
+            var serverName = "localhost:80";
+
+            try
+            {
+                serverName = File.ReadAllText("connect.config").Trim();
+            }
+            catch (Exception e)
+            {
+                ExceptionLogger.Log(e);
+            }
+
+            return serverName;
+        }
     }
 }
